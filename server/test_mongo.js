0ccba7b95c8b7977110be81089592eb1652ab842
ajayaysqo <ajayaysqo@gmail.com>
2025-11-02 14:29:55 +0530

    hello

--- server/test_mongo.js
+++ server/test_mongo.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
-
-// const MONGODB_URI = 'mongodb+srv://admin:vHeUpvZbzRu6RokS@cluster0.abc123z.mongodb.net/job_importer?retryWrites=true&w=majority';
-require('dotenv').config();
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = 'mongodb+srv://ajayvemala02_db_user:aYSFZtiuYEmKMKXS@cluster0.ulfyey2.mongodb.net/job_importer?retryWrites=true&w=majority';
+// require('dotenv').config();
+// const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose.connect(MONGODB_URI)
   .then(() => {
